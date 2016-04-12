50183eaadbec50331407a23918dcfd75663e3b98
Maurizio-Mario <maurizio.murino@hotmail.com>
2016-04-12 19:09:44 +0200

    z_plot function completed. Pending: minor fixes to p==3 probabilities calculations.

--- EasieR/Easier.Rcheck/00_pkg_src/Easier/R/EasieR.R
+++ EasieR/Easier.Rcheck/00_pkg_src/Easier/R/EasieR.R
@@ -2,23 +2,24 @@
 #'
 #' This function is thought for the chapter 6 on Standard Normal distributions. It allows to rapidly draw a plot with Lattice representing a Standard normal distribution translating the inputs for a generic normal distribution. Furthermore, it allows to stress out cumulative probability distributions if necessary.
 #'
-#' @param x is the score of interest from a given distribution.
+#' @param my_x is the score of interest from a given distribution.
 #' @param myMean is the mean of the given distribution.
 #' @param mySd is the standard deviation of the given distribution.
-#' @param p default zero, it is called for plotting and calculating
-#' @param main it should give a desired title to the plot
+#' @param p default zero, it is called for plotting and calculating "less" P(x<a), "more" P(x>a), "between" P(a<x<b)
+#' @param y second score for a "probability between x and y" problem
+#' @param main it gives give a desired title to the plot
 #' @return a plot stressing the corresponding z-score of the input data.
 #' @author Maurizio Mario Murino
-#' @details the function is actually under development.
+#' @details the package is actually under development.
 #' @seealso \code{Lattice, xyplot}
 #' @export
 #' @importFrom lattice xyplot
 #'
 
-z_plot <- function(x, myMean, mySd, p = 0, title = "", y){
+z_plot <- function(my_x, myMean, mySd, p = 0, y, title = ""){
     dt1 <- seq(-4, 4, length = 1000)
     dt2 <- dnorm(dt1, 0, 1)
-    z <- (x - myMean)/mySd
+    z <- (my_x - myMean)/mySd
     if(p == 3){
         z2 <- (y - myMean)/mySd
     }
@@ -27,30 +28,37 @@ z_plot <- function(x, myMean, mySd, p = 0, title = "", y){
            main = title,
            panel = function(x, y, ...){
                panel.xyplot(x, y, ...)
+               p2 <- round(pnorm(my_x, myMean, mySd), 3)
+               p3 <- round(pnorm(y, myMean, mySd), 3)
                if(p == 1){
                    xx <- c(-4, x[x>=-4 & x<=z], z)
                    yy <- c(0, y[x>=-4 & x<=z], 0)
                    panel.polygon(xx,yy, ..., col='red')
+                   panel.text(lab = paste("P(X<a) = ", p2, sep = ""), x = 3, y = 0.35)
 
                }else if(p == 2){
                    xx <- c(z, x[x>=z & x<=4], 4)
                    yy <- c(0, y[x>=z & x<=4], 0)
                    panel.polygon(xx,yy, ..., col='red')
+                   panel.text(lab = paste("P(X>a) = ",(1 - p2), sep = ""), x = 3, y = 0.35)
                }else if(p == 3){
                    panel.abline(v = z2, lty = 2)
                    if(z < z2){
                        xx <- c(z, x[x>=z & x<=z2], z2)
                        yy <- c(0, y[x>=z & x<=z2], 0)
                        panel.polygon(xx,yy, ..., col='red')
+                       panel.text(lab = paste("P(a<X<b) = ",(p3 - p2), sep = ""), x = 3, y = 0.35)
 
                    }else{
                        xx <- c(z2, x[x>=z2 & x<=z], z)
                        yy <- c(0, y[x>=z2 & x<=z], 0)
                        panel.polygon(xx,yy, ..., col='red')
+                       panel.text(lab = paste("P(a<X<b) = ",(p2 - p3), sep = ""), x = 3, y = 0.35)
                    }
                }
                panel.abline(v = c(z, 0), lty = c(2, 3))
-               panel.text(lab = c(paste("z = ", z, sep = ""), "Mean"), y = c(.41, .4), x = c(z, 0)+.2, cex = 1)
+               panel.text(lab = c(paste("z = ", z, sep = ""), paste("z2 = ", z2, sep = "")), y = c(.41, .39), x = c(z, z2), cex = 1)
+
            }
     )
 }
