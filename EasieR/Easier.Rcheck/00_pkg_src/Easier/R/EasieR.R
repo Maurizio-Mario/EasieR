234090b843a5015908736d37c0f23a66ee826ac7
Maurizio-Mario <maurizio.murino@hotmail.com>
2016-04-15 17:34:45 +0200

    Minor fixes. z_plot renamed zPlot. Probability label adjusted.

--- EasieR/Easier.Rcheck/00_pkg_src/Easier/R/EasieR.R
+++ EasieR/Easier.Rcheck/00_pkg_src/Easier/R/EasieR.R
@@ -7,7 +7,6 @@
 #' @param mySd is the standard deviation of the given distribution.
 #' @param p default zero, it is called for plotting and calculating "less" P(x<a), "more" P(x>a), "between" P(a<x<b)
 #' @param y second score for a "probability between x and y" problem
-#' @param main it gives give a desired title to the plot
 #' @return a plot stressing the corresponding z-score of the input data.
 #' @author Maurizio Mario Murino
 #' @details the package is actually under development.
@@ -16,7 +15,7 @@
 #' @importFrom lattice xyplot
 #'
 
-z_plot <- function(my_x, myMean, mySd, p = 0, y, title = ""){
+z_plot <- function(my_x, myMean, mySd, p = 0, y){
     dt1 <- seq(-4, 4, length = 1000)
     dt2 <- dnorm(dt1, 0, 1)
     z <- (my_x - myMean)/mySd
@@ -47,17 +46,21 @@ z_plot <- function(my_x, myMean, mySd, p = 0, y, title = ""){
                        xx <- c(z, x[x>=z & x<=z2], z2)
                        yy <- c(0, y[x>=z & x<=z2], 0)
                        panel.polygon(xx,yy, ..., col='red')
-                       panel.text(lab = paste("P(a<X<b) = ",(p3 - p2), sep = ""), x = 3, y = 0.35)
+                       panel.text(lab = paste("P(a<X<b) = ",abs(p3 - p2), sep = ""), x = 3, y = 0.35)
 
                    }else{
                        xx <- c(z2, x[x>=z2 & x<=z], z)
                        yy <- c(0, y[x>=z2 & x<=z], 0)
                        panel.polygon(xx,yy, ..., col='red')
-                       panel.text(lab = paste("P(a<X<b) = ",(p2 - p3), sep = ""), x = 3, y = 0.35)
+                       panel.text(lab = paste("P(a<X<b) = ",abs(p2 - p3), sep = ""), x = 3, y = 0.35)
                    }
                }
                panel.abline(v = c(z, 0), lty = c(2, 3))
-               panel.text(lab = c(paste("z = ", z, sep = ""), paste("z2 = ", z2, sep = "")), y = c(.41, .39), x = c(z, z2), cex = 1)
+               if(p == 3){
+                   panel.text(lab = c(paste("z = ", z, sep = ""), paste("z2 = ", z2, sep = "")), y = c(.41, .39), x = c(z, z2), cex = 1)
+               }else{
+               panel.text(lab = paste("z = ", z, sep = ""), y = .41, x = z2, cex = 1)
+               }
 
            }
     )
--- EasieR/Easier/R/EasieR.R
+++ EasieR/Easier/R/EasieR.R
@@ -7,7 +7,6 @@
 #' @param mySd is the standard deviation of the given distribution.
 #' @param p default zero, it is called for plotting and calculating "less" P(x<a), "more" P(x>a), "between" P(a<x<b)
 #' @param y second score for a "probability between x and y" problem
-#' @param main it gives give a desired title to the plot
 #' @return a plot stressing the corresponding z-score of the input data.
 #' @author Maurizio Mario Murino
 #' @details the package is actually under development.
@@ -16,53 +15,47 @@
 #' @importFrom lattice xyplot
 #'
 
-z_plot <- function(my_x, myMean, mySd, p = 0, y, title = ""){
+zPlot <- function(my_x, myMean, mySd, p = 0, y){
     dt1 <- seq(-4, 4, length = 1000)
     dt2 <- dnorm(dt1, 0, 1)
-    z <- (my_x - myMean)/mySd
+    z <- round(((my_x - myMean)/mySd), 3)
     if(p == 3){
-        z2 <- (y - myMean)/mySd
+        z2 <- round(((y - myMean)/mySd), 3)
+        p3 <- round(pnorm(y, myMean, mySd), 3)
     }
     xyplot(dt2 ~ dt1,
            type = "l",
-           main = title,
            panel = function(x, y, ...){
                panel.xyplot(x, y, ...)
                p2 <- round(pnorm(my_x, myMean, mySd), 3)
-               p3 <- round(pnorm(y, myMean, mySd), 3)
+               panel.abline(v = c(z, 0), lty = c(2, 3))
+               panel.text(lab = paste("z = ", z, sep = ""), x = z, y = .41)
                if(p == 1){
                    xx <- c(-4, x[x>=-4 & x<=z], z)
                    yy <- c(0, y[x>=-4 & x<=z], 0)
                    panel.polygon(xx,yy, ..., col='red')
-                   panel.text(lab = paste("P(X<a) = ", p2, sep = ""), x = 3, y = 0.35)
+                   panel.text(lab = paste("P(X<a) = ", p2, sep = ""), x = 2.8, y = 0.35)
 
                }else if(p == 2){
                    xx <- c(z, x[x>=z & x<=4], 4)
                    yy <- c(0, y[x>=z & x<=4], 0)
                    panel.polygon(xx,yy, ..., col='red')
-                   panel.text(lab = paste("P(X>a) = ",(1 - p2), sep = ""), x = 3, y = 0.35)
+                   panel.text(lab = paste("P(X>a) = ",(1 - p2), sep = ""), x = 2.8, y = 0.35)
                }else if(p == 3){
                    panel.abline(v = z2, lty = 2)
                    if(z < z2){
                        xx <- c(z, x[x>=z & x<=z2], z2)
                        yy <- c(0, y[x>=z & x<=z2], 0)
                        panel.polygon(xx,yy, ..., col='red')
-                       panel.text(lab = paste("P(a<X<b) = ",abs(p3 - p2), sep = ""), x = 3, y = 0.35)
+                       panel.text(lab = paste("P(a<X<b) = ",abs(p2 - p3), sep = ""), x = 2.8, y = 0.35)
 
                    }else{
                        xx <- c(z2, x[x>=z2 & x<=z], z)
                        yy <- c(0, y[x>=z2 & x<=z], 0)
                        panel.polygon(xx,yy, ..., col='red')
-                       panel.text(lab = paste("P(a<X<b) = ",abs(p2 - p3), sep = ""), x = 3, y = 0.35)
+                       panel.text(lab = paste("P(a<X<b) = ",abs(p2 - p3), sep = ""), x = 2.8, y = 0.35)
                    }
                }
-               panel.abline(v = c(z, 0), lty = c(2, 3))
-               if(p == 3){
-                   panel.text(lab = c(paste("z = ", z, sep = ""), paste("z2 = ", z2, sep = "")), y = c(.41, .39), x = c(z, z2), cex = 1)
-               }else{
-               panel.text(lab = paste("z = ", z, sep = ""), y = .41, x = z2, cex = 1)
-               }
-
            }
     )
 }
